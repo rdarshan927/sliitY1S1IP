3090ed24e6cc2cb087c5a3237a477b66e70a3de4
rdarshan927 <rdarshan927@gmail.com>
2024-04-07 09:57:19 +0530

    corrected version 2.0

--- 2019/Question3.c
+++ 2019/Question3.c
@@ -3,24 +3,24 @@
 #include<math.h>
 
 float calcAnnualInterest(float interestRate, float amount);
-float findTotalAmount(float interest, float amount);
+float findTotalAmount(float interestRate, float amount);
 void testTotalAmount();
 
 int main(void)
 {
-    float annualIntRate, amount, interest;
+    float interestRate, amount;
     int i;
 
     printf("Enter the amount : ");
     scanf("%f", &amount);
     printf("Enter the Annual interest rate : ");
-    scanf("%f", &annualIntRate);
+    scanf("%f", &interestRate);
+
+    testTotalAmount();
 
     for(i=0; i<5; i++)
     {
-        interest = calcAnnualInterest(annualIntRate, amount);
-        amount = findTotalAmount(interest, amount);
-        testTotalAmount();
+        amount = findTotalAmount(interestRate, amount);
         printf("Total amount earned at the end of %d year : %.2f\n", i+1, amount);
     }
 
@@ -35,13 +35,62 @@ float calcAnnualInterest(float interestRate, float amount)
         return amount*interestRate/100.0;
 }
 
-float findTotalAmount(float interest, float amount)
+float findTotalAmount(float interestRate, float amount)
 {
-    return interest+amount;
+    return (calcAnnualInterest(interestRate, amount)+amount);
 }
 
 void testTotalAmount()
 {
-    assert(fabs(findTotalAmount(1000, 10000) - 11000) < 0.0001);
-    assert(fabs(findTotalAmount(100000, 1000000) - 1100000) < 0.0001);
+    assert(fabs(findTotalAmount(10, 10000) - 11000) < 0.0001);
+    assert(fabs(findTotalAmount(10, 1000000) - 1100000) < 0.0001);
 }
+
+
+// #include<stdio.h>
+// #include<assert.h>
+// #include<math.h>
+
+// float calcAnnualInterest(float interestRate, float amount);
+// float findTotalAmount(float interest, float amount);
+// void testTotalAmount();
+
+// int main(void)
+// {
+//     float annualIntRate, amount, interest;
+//     int i;
+
+//     printf("Enter the amount : ");
+//     scanf("%f", &amount);
+//     printf("Enter the Annual interest rate : ");
+//     scanf("%f", &annualIntRate);
+
+//     for(i=0; i<5; i++)
+//     {
+//         interest = calcAnnualInterest(annualIntRate, amount);
+//         amount = findTotalAmount(interest, amount);
+//         testTotalAmount();
+//         printf("Total amount earned at the end of %d year : %.2f\n", i+1, amount);
+//     }
+
+//     return 0;
+// }
+
+// float calcAnnualInterest(float interestRate, float amount)
+// {
+//     if(amount>1000000.0)
+//         return amount*((interestRate+0.5)/100.0);
+//     else
+//         return amount*interestRate/100.0;
+// }
+
+// float findTotalAmount(float interest, float amount)
+// {
+//     return interest+amount;
+// }
+
+// void testTotalAmount()
+// {
+//     assert(fabs(findTotalAmount(1000, 10000) - 11000) < 0.0001);
+//     assert(fabs(findTotalAmount(100000, 1000000) - 1100000) < 0.0001);
+// }
